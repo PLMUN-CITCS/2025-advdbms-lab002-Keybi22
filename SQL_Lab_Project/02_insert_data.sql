3550a085f3779c805c9d709bef5e08c04b8dbd54
Kim Britney Surigao <surigaokimbritney_bsit@plmun.edu.ph>
2025-02-10 01:45:14 +0800

    Delete SQL_Lab_Project directory

--- SQL_Lab_Project/02_insert_data.sql
+++
@@ -1,6 +0,0 @@
--- Creating tblStudents table
--- Inserting data into tblStudents
-INSERT INTO tblStudents (student_name, student_email, student_phone)
-VALUES
-    ('Kim Britney Surigao', 'kimbrineysurigao@gmail.com', '09667819765'),
-    ('Kim Surigao', 'kimbritneysurigao@gmail.com', '09667819765');

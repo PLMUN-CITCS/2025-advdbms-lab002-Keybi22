ec2bd3b76a5f437c15a586c380b877d42a1e16a5
Kim Britney Surigao <surigaokimbritney_bsit@plmun.edu.ph>
2025-02-10 01:42:11 +0800

    01_create_tables.sql

--- SQL_Lab_Project/01_create_tables.sql
+++ SQL_Lab_Project/01_create_tables.sql
@@ -1,7 +1,7 @@
-CREATE TABLE tblstudents (
+CREATE TABLE tblStudents (
     student_id INT PRIMARY KEY AUTO_INCREMENT,
     student_name VARCHAR(50) NOT NULL,
     student_email VARCHAR(50) NOT NULL UNIQUE,
     student_phone VARCHAR(50) NOT NULL
 );
-
+-- This script creates the tblStudents table with basic student details.

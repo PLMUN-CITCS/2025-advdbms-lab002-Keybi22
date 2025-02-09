406d8c963fa72791efd89d573c4113a49c422250
Kim Britney Surigao <surigaokimbritney_bsit@plmun.edu.ph>
2025-02-10 01:43:43 +0800

    02_insert_data.sql

--- SQL_Lab_Project/02_insert_data.sql
+++ SQL_Lab_Project/02_insert_data.sql
@@ -4,4 +4,3 @@ INSERT INTO tblStudents (student_name, student_email, student_phone)
 VALUES
     ('Kim Britney Surigao', 'kimbrineysurigao@gmail.com', '09667819765'),
     ('Kim Surigao', 'kimbritneysurigao@gmail.com', '09667819765');
-    
